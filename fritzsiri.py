93638a855f3c37b25a9751fb21d3162c97110f2e
Jan Heitkötter <jan@heitkoetter.net>
2021-01-05 19:53:22 +0100

    Translation, licence, config template

--- fritzsiri.py
+++ fritzsiri.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
 
+"""Switch Fritz!Box wireless guest access and SmartHome devices.
+
+Copyright (C) 2021 Jan Heitkötter <jan@heitkoetter.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import itertools
+from os import access, R_OK, path
 from configparser import ConfigParser
 from fritzconnection import FritzConnection
 from fritzconnection.lib.fritzwlan import FritzWLAN
@@ -9,9 +28,10 @@ from fritzconnection.lib.fritzhomeauto import FritzHomeAutomation
 from fritzconnection.core.exceptions import FritzServiceError
 import argparse
 
+
 def find_guest_wlan(fwlan, ssid):
-    # finds service number of guest WLAN
-    # returns None if no guest WLAN found
+    # Servicenummer des Gäste-WLAN anhand der SSID finden (normalerweise 3)
+    # None, falls kein Gäste-WLAN vorhanden
     for n in itertools.count(1):
         fwlan.service = n
         try:
@@ -20,76 +40,87 @@ def find_guest_wlan(fwlan, ssid):
         except FritzServiceError:
             return None
 
+
 def find_ains(fha):
-    # finds SmartHome actors
-    # returns empty dict if no actors availble
+    # SmartHome-Geräte finden
+    # leeres Dict, falls keine vorhanden
     device_dict = {}
     for device in fha.device_informations():
         device_dict[device["NewDeviceName"]] = device["NewAIN"]
     return device_dict
 
+
 if __name__ == "__main__":
-    # read config
+    # Konfiguration auslesen
+    if not access(path=path.expanduser("~/.fritzsiri.ini"), mode=R_OK):
+        print("FATAL: config file not found")
+        exit(1)
     config = ConfigParser()
-    config.read("fritzsiri.ini")
+    config.read(path.expanduser("~/.fritzsiri.ini"))
     fritz_address = config["fritzbox"]["address"]
     fritz_user = config["fritzbox"]["user"]
     fritz_password = config["fritzbox"]["password"]
+    guest_ssid = config["wireless"]["guestssid"]
 
-    # valid values for arguments
-    services = {"Gäste-WLAN", "SmartHome"}
-    schalter = {"aus": "OFF", "an": "ON", "Wechsel": "TOGGLE"}
+    # zulässige Argumente
+    services = ["SmartHome", "WirelessGuestAccess"]
+    schalter = {"off": "OFF", "on": "ON", "toggle": "TOGGLE"}
+    # zulässige SmartHome-Geräte auslesen
+    print("Reading SmartHome devices from Fritz!Box")
     fha = FritzHomeAutomation(address=fritz_address, user=fritz_user, password=fritz_password)
-    ain = find_ains(fha)
+    geraete = find_ains(fha)
 
-    # create argument parser
-    parser = argparse.ArgumentParser(description="Schalte Aktoren der Fritz!Box.")
-    parser.add_argument("service", help="Name des Service", choices=services)
-    parser.add_argument("-a", "--aktor", help="nur Service SmartHome: Name des zu schaltenden Aktors",
-                        choices=ain, default=None)
-    parser.add_argument("-s", "--stellung", help="WLAN oder Aktor schalten",
-                        choices=schalter, default="Wechsel")
+    # Argument Parser erzeugen
+    parser = argparse.ArgumentParser(description="Simple Fritz!Box switching.")
+    parser.add_argument("service", help="Service Name", choices=services)
+    parser.add_argument("-d", "--device", help="Service SmartHome: device name",
+                        choices=geraete, default=None)
+    parser.add_argument("-s", "--switch", help="Switching operation",
+                        choices=schalter, default="toggle")
     args = parser.parse_args()
-    print(args.service)
 
     # API connection
     fc = FritzConnection(address=fritz_address, user=fritz_user, password=fritz_password)
 
-    if args.service == "Gäste-WLAN":
-        # find out service number of guest WLAN (usually 3)
+    if args.service == "WirelessGuestAccess":
+        # Servicenummer des Gäste-WLAN finden
+        print("Searching for wireless guest access with SSID", guest_ssid)
         fwlan = FritzWLAN(address=fritz_address, user=fritz_user, password=fritz_password)
-        service = find_guest_wlan(fwlan, config["wlan"]["guestssid"])
+        service = find_guest_wlan(fwlan, guest_ssid)
         if service is None:
-            print("No guest WLAN found")
+            print("FATAL: no wireless guest access found")
             exit(1)
         fwlan.service = service
-        # read current state of guest WLAN
+        # aktuellen Zustand lesen
         old_enable = fc.call_action(service_name="WLANConfiguration" + str(fwlan.service),
                                     action_name="GetInfo")["NewEnable"]
-        # switching logic
-        if args.stellung == "an":
+        # neuen Zustand bestimmen
+        if args.switch == "on":
             new_enable = True
-        elif args.stellung == "aus":
+        elif args.switch == "off":
             new_enable = False
         else:
             new_enable = not old_enable
-        print(old_enable, "->", new_enable)
         if old_enable != new_enable:
+            tofo = {True: "on", False: "off"}
+            print("Switching wireless guest access from", tofo[old_enable], "to", tofo[new_enable])
             fc.call_action(service_name="WLANConfiguration" + str(fwlan.service),
                            action_name="SetEnable", NewEnable=new_enable)
-    elif args.service == "SmartHome":
-        # simpler logic with built-in toggle
-        if ain == {}:
-            print("No smarthome actors available")
-            exit(1)
-        if fc.call_action(service_name="X_AVM-DE_Homeauto1",
-                          action_name="GetSpecificDeviceInfos",
-                          NewAIN=ain[args.aktor])["NewSwitchIsEnabled"] == "DISABLED":
-            print("Actor doesn’t allow switching")
-            exit(1)
-        if args.aktor is not None:
-            print(args.aktor, args.stellung)
-            fc.call_action(service_name="X_AVM-DE_Homeauto1", action_name="SetSwitch",
-                           NewAIN=ain[args.aktor], NewSwitchState=schalter[args.stellung])
         else:
-            print("missing actor")
+            print("No change in wireless guest access required")
+    elif args.service == "SmartHome":
+        # einfachere Schaltlogik, weil es hier TOGGLE gibt
+        if geraete == {}:
+            print("FATAL: no SmartHome devices available")
+            exit(1)
+        if args.device is not None:
+            if fc.call_action(service_name="X_AVM-DE_Homeauto1",
+                              action_name="GetSpecificDeviceInfos",
+                              NewAIN=geraete[args.device])["NewSwitchIsEnabled"] == "DISABLED":
+                print("FATAL: device does not allow switching")
+                exit(1)
+            print("New switch state for device", args.device, ":", args.switch)
+            fc.call_action(service_name="X_AVM-DE_Homeauto1", action_name="SetSwitch",
+                           NewAIN=geraete[args.device], NewSwitchState=schalter[args.switch])
+        else:
+            print("FATAL: device argument missing")
